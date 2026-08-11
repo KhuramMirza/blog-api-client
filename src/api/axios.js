bf63fab62faf587cc6616ec4947b510977166aea
Khuram Mirza <mirzakhuram101@gmail.com>
2026-08-11 19:29:25 +0500

    fix: remove the jwt localStorage approach and use cookie approach

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,18 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.BACKEND_API_URL,
+  baseURL: import.meta.env.VITE_BACKEND_API_URL,
+  withCredentials: true,
 });
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
 
 export default api;
